b3a2fa61b7cc03e2fe743b22d10372d160ff017a
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-11-07 04:02:53 -0300

    expressoes regulares e inicio da logica do codigo, alem dos erros de marcacao

--- ENTRADA/Code.py
+++ ENTRADA/Code.py
@@ -1,9 +1,24 @@
-class Code:
+from COMPUTADOR import Constantes as Consts
 
+
+class Code:
+    """
+    "end": "end" 1
+    "label": r"(label)\s+([^0]\d*)", 2
+    "inc": r"(inc)\s+(\w+)\s*", 2
+    "dec": r"(dec)\s+(\w+)\s*", 2
+    "add": r"(add)\s+(\w+)\s*,\s*(\w+)\s*", 3
+    "mov": r"(mov)\s+(\w+)\s*,\s*(\w+)\s*", 3
+    "imul": r"(imul)\s+(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*", 4
+    "condicao": r"(\w+)\s*(<>|<|>|==)\s*(\w+)\s*[?]\s*(?:jump\s+|)(0|\d+)\s*:\s*(?:jump\s+|)(0|\d+)", 5
+    """
     def __init__(self, groups):
         self.groups = groups
         self.byte_array = Code.get_byte_array(groups)
 
     @staticmethod
     def get_byte_array(groups):
+        code = [-1 for i in range(Consts.CODE_SIZE)]
+    reconhecer a instrucao lá fora e aqui só reconhecer os valores restantes
+
         return [groups]
--- ENTRADA/Regex.py
+++ ENTRADA/Regex.py
@@ -22,20 +22,24 @@ class Regex:
             "dec": r"(dec)\s+(\w+)\s*",
             "label": r"(label)\s+([^0]\d*)",
             "condicao": r"(\w+)\s*(<>|<|>|==)\s*(\w+)\s*[?]\s*(?:jump\s+|)(0|\d+)\s*:\s*(?:jump\s+|)(0|\d+)",
-            "end": "end code"
+            "end": "end"
         }
 
         for line in lines:
             instrucao = operacao.match(line).group(1)
+            reresult = None
+            codetype = None
             if instrucao in patterns:
-                reresult = re.match(patterns[instrucao], line)
+                reresult = re.match(patterns[instrucao], line)[1::]
+                codetype = instrucao
             else:
-                reresult = re.match(patterns["condicao"], line)
+                reresult = re.match(patterns["condicao"], line)[1::] aqui
+                codetype = "condicao"
 
             if reresult is None:
                 raise SyntaxError(line)
 
-            codes.append(Code(reresult))
+            codes.append(Code(reresult.groups()))
 
             if instrucao == "end":
                 break
